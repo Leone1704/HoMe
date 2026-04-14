ce5676129a5fe801ab176d1f148eb40c0611aa80
Lachs <121566852+Leone1704@users.noreply.github.com>
2026-04-14 02:51:07 +0200

    Delete Software_Eng1 directory

--- Software_Eng1/Übung_5/Shape.java
+++
@@ -1,9 +0,0 @@
-public abstract class Shape {
-    public abstract double area();
-    public abstract double perimeter();
-
-    @Override
-    public String toString() {
-        return String.format("%s[area=%.2f, perimeter=%.2f]", this.getClass().getSimpleName(), area(), perimeter());
-    }
-}
