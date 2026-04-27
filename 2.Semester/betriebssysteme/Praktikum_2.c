74de61e0ef1a53e9d524b4c3ac7ab2d8a50ebfd9
Leon Heilmann <leone.heilmann@icloud.com>
2026-04-28 01:35:24 +0200

    Praktikum 2

--- 2.Semester/betriebssysteme/Praktikum_2.c
+++ 2.Semester/betriebssysteme/Praktikum_2.c
@@ -3,9 +3,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <sys/mman.h>
-
-
+#include <sys/stat.h>
+static const char *SHM_NAME = "/praktikum_2_zaehler";
 
 int aufgabe_1(){
     pid_t pid = fork();
@@ -78,26 +79,52 @@ void child_funktion_aufgabe_3(int *zaehler_ptr) {
 }
 
 int aufgabe_3() {
-    int *zaehler_ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (zaehler_ptr == MAP_FAILED) {
-        perror("mmap fehlgeschlagen");
+    shm_unlink(SHM_NAME);
+
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0600);
+    if (shm_fd == -1) {
+        perror("shm_open fehlgeschlagen");
         return 1;
     }
 
-    *zaehler_ptr = 0;
+    if (ftruncate(shm_fd, sizeof(int)) == -1) {
+        perror("ftruncate fehlgeschlagen");
+        close(shm_fd);
+        shm_unlink(SHM_NAME);
+        return 1;
+    }
 
     for (int i = 0; i < 10; i++) {
         pid_t pid = fork();
 
         if (pid < 0) {
             perror("fork fehlgeschlagen");
-            munmap(zaehler_ptr, sizeof(int));
+            close(shm_fd);
+            shm_unlink(SHM_NAME);
             return 1;
         }
 
         if (pid == 0) {
+            int child_shm_fd = shm_open(SHM_NAME, O_RDWR, 0600);
+            if (child_shm_fd == -1) {
+                perror("child shm_open fehlgeschlagen");
+                exit(67);
+            }
+
+            int *zaehler_ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, child_shm_fd, 0);
+            if (zaehler_ptr == MAP_FAILED) {
+                perror("child mmap fehlgeschlagen");
+                close(child_shm_fd);
+                exit(67);
+            }
+
             child_funktion_aufgabe_3(zaehler_ptr);
-            munmap(zaehler_ptr, sizeof(int));
+
+            if (munmap(zaehler_ptr, sizeof(int)) == -1) {
+                perror("child munmap fehlgeschlagen");
+            }
+
+            close(child_shm_fd);
             exit(67);
         }
     }
@@ -106,8 +133,22 @@ int aufgabe_3() {
         wait(NULL);
     }
 
+    int *zaehler_ptr = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (zaehler_ptr == MAP_FAILED) {
+        perror("parent mmap fehlgeschlagen");
+        close(shm_fd);
+        shm_unlink(SHM_NAME);
+        return 1;
+    }
+
     printf("Parent PID=%d (SHM): Wert=%d, Adresse=%p\n", getpid(), *zaehler_ptr, (void *)zaehler_ptr);
-    munmap(zaehler_ptr, sizeof(int));
+
+    if (munmap(zaehler_ptr, sizeof(int)) == -1) {
+        perror("parent munmap fehlgeschlagen");
+    }
+
+    close(shm_fd);
+    shm_unlink(SHM_NAME);
     return 0;
 }
 
